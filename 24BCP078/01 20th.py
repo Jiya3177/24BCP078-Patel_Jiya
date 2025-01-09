557b29374499d8c977a01fb81454816c20d04a5d
Jiya3177 <chaudharyjilu3177@gmail.com>
2025-01-10 01:50:04 +0530

    01 20th.py

--- 24BCP078/01 20th.py
+++ 24BCP078/01 20th.py
@@ -1,8 +1,5 @@
-# Calculate net salary where net salary = gross salary + allowance - deduction.
-# Allowances are 10% while deductions are 3% of gross salary. 
-salary = float(input("enter salary: "))
-allowances = salary * 0.1
-deduction = salary * 0.03
-
-net_salary = salary + allowances - deduction
-print("net salary is: ", net_salary)  
+# calculate area of triangle
+l = float(input("Enter lenght of triangle: "))
+w = float(input("Enter width of triangle: "))
+area =  (l * w)/2
+print("Area of triangle is: ", area)
