65e8dc5db35436efbf48821b6f355bed4e06804b
Jiya3177 <chaudharyjilu3177@gmail.com>
2025-01-10 02:00:15 +0530

    01 10th.py

---
+++ 24BCP078/01 10th.py
@@ -0,0 +1,6 @@
+# Convert dollars into pound where 1 $ = 48 Rs. And 1 pound = 70 Rs.
+d = float(input("enter Dollar$: "))
+R = d * 48
+print(d, "$ = ", R ,"Rs.")
+p = R / 70
+print(R, "Rs. = ", p, "pound")
