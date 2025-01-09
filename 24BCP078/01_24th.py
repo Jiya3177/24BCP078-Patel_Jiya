0b7b6dbffbe051f91b51d687b67a8c0cb97c8c6c
Jiya3177 <chaudharyjilu3177@gmail.com>
2025-01-10 01:34:40 +0530

    Add files via upload

---
+++ 24BCP078/01_24th.py
@@ -0,0 +1,7 @@
+# WAP to swap 2 numbers
+num1 = float(input("enter a num: "))
+num2 = float(input("enter a num: "))
+print("Before swapping num1 =", num1, "num2 =", num2)
+num1, num2 = num2, num1
+print("After swapping num1 =", num1, "num2 =", num2)
+
