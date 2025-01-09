bffacad4ee566e1d28960d29fc115fe65284555c
Jiya3177 <chaudharyjilu3177@gmail.com>
2025-01-10 01:40:46 +0530

    01 16th.py

--- 24BCP078/01 16th.py
+++ 24BCP078/01 16th.py
@@ -1,11 +1,4 @@
-# # Calculate interest where I = PRN/100.
-# Input :  = 10000
-#         R = 5
-#         T = 5
-# Output :2500.0
-# We need to find simple interest on 
-# Rs. 10,000 at the rate of 5% for 5 
-# units of time.
+# Calculate interest where I = PRN/100.
 p = float(input("principale value: "))
 t = float(input("enter time: "))
 n = float(input("the rate of interest is: "))
