95b488aebec7b682eecd6b8c1a270c6bd49e0e61
Jiya3177 <chaudharyjilu3177@gmail.com>
2025-01-10 01:49:44 +0530

    01 21.py

--- 24BCP078/01 21.py
+++ 24BCP078/01 21.py
@@ -1,5 +1,8 @@
-# Calculate net sales where net sales = gross sales - 10% discount of gross
-gross = float(input("enter gross sales: "))
-discount = 0.1 * gross
-net = gross - discount
-print("net sales: ", net)
+# Calculate net salary where net salary = gross salary + allowance - deduction.
+# Allowances are 10% while deductions are 3% of gross salary. 
+salary = float(input("enter salary: "))
+allowances = salary * 0.1
+deduction = salary * 0.03
+
+net_salary = salary + allowances - deduction
+print("net salary is: ", net_salary)  
