205f4c59972c4b069be371b5a5dda856bd970169
Jiya3177 <chaudharyjilu3177@gmail.com>
2025-01-10 01:27:59 +0530

    Add files via upload

---
+++ 24BCP078/01 18th.py
@@ -0,0 +1,10 @@
+# calculate area & perimeter of a rectangle. A = L*B, P = 2 (L+B)
+
+l = float(input("Enter length of rectangle: "))
+b = float(input("Enter width of rectangle: "))
+
+area = l * b
+perimeter = 2 * (l + b)
+print("Area of rectangle is: ", area)
+print("Perimeter of rectangle is: ", perimeter)  
+
