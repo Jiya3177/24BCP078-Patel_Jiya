fbd22730e31a27887e7bcac72d3ec44b2fa3d5af
Patel Jiya <chaudharyjilu3177@gmail.com>
2025-03-13 15:45:03 +0530

    Delete 09 functions/1.py

--- 09 functions/1.py
+++
@@ -1,9 +0,0 @@
-def recgcd(n, d):
-    if n % d == 0:
-        return d
-    else:
-        return recgcd(d, n % d)
-
-n = int(input("Enter a number.:"))
-r = int(input("Enter another number.:"))
-print ("GCD of ",n,"and",r,"is",recgcd(n,r))
